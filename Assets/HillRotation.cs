6506f28314ae616e9aabd7dd5556eac7b3b681db
Andrew Pitman <drewpit@gmail.com>
2019-08-03 17:27:17 -0400

    Add rotationEffects in Hill zones

---
+++ Assets/HillRotation.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HillRotation : MonoBehaviour
+{
+    public float rotationAmount = 45f;
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float runSpeed = 5f;
+    public float rotationSpeed = 5f;
 
     private float horizontalMove = 0f;
+    private float desiredRotation = 0f;
+    private int activeRotations = 0;
 
     // Update is called once per frame
     void Update()
@@ -18,5 +21,38 @@ public class PlayerMovement : MonoBehaviour
     {
         float newXPos = transform.position.x + horizontalMove * Time.fixedDeltaTime;
         transform.position = new Vector3(newXPos, transform.position.y, transform.position.z);
+
+        // Note: This is apparently not the right way to do rotations in general, but it works for me around 1 axis so there
+        float currentRotation = transform.rotation.eulerAngles.z;
+        float desiredRotationRemaining = desiredRotation - currentRotation;
+        float smoothRotation = desiredRotationRemaining * rotationSpeed * Time.deltaTime;
+        if (Mathf.Abs(desiredRotationRemaining) > 0.01f) // Give up once we get close enough
+        {
+            // Debug.Log("Current rotation is " + currentRotation + " so we are rotating " + smoothRotation);
+            transform.Rotate(0f, 0f, smoothRotation);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("rotationEffect"))
+        {
+            activeRotations++;
+            Debug.Log("ActiveRotations =" + activeRotations + "\nCollided with a rotationEffect and using this as desired rotation amount:" + collision.gameObject.GetComponent<HillRotation>().rotationAmount);
+            desiredRotation = collision.gameObject.GetComponent<HillRotation>().rotationAmount;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("rotationEffect"))
+        {
+            Debug.Log("Leaving collision zone");
+            activeRotations--;
+            if (activeRotations == 0)
+            {
+                Debug.Log("Setting desiredRotation to 0");
+                desiredRotation = 0f;
+            }
+        }
     }
 }
