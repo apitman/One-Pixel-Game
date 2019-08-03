7def0610fc469863882cd4a5d437a49a63be5e57
Andrew Pitman <drewpit@gmail.com>
2019-08-03 14:25:08 -0400

    Add player and camera movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float runSpeed = 5f;
+
+    private float horizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        float newXPos = transform.position.x + horizontalMove * Time.fixedDeltaTime;
+        transform.position = new Vector3(newXPos, transform.position.y, transform.position.z);
+    }
+}
