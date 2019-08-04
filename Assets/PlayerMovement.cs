74afac105c6b4a7f3bc2c9c542e11a1a3b40c775
Andrew Pitman <drewpit@gmail.com>
2019-08-04 09:50:15 -0400

    Add a bunch of text and make the pixel fade out at the end

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,13 +11,28 @@ public class PlayerMovement : MonoBehaviour
     public bool controlsEnabled = true;
     public GameObject textPrefab;
     public float chainTextDelay = 4f;
+    public float minXPos = -16f;
+    public float yOffsetForText = 180f;
 
     private float horizontalMove = 0f;
     private float desiredRotation = 0f;
     private int activeRotations = 0;
     private bool playerHasMovedEver = false;
-    private bool text1Created = false;
+    private bool text1Created = false; // Don't @ me for these text*Created Booleans
+    private bool textLeftBoundCreated = false;
+    private bool textHillCreated = false;
+    private bool textSteeperCreated = false;
+    private bool textSteepestCreated = false;
+    private bool textMadeItCreated = false;
+    private bool textDownhillCreated = false;
+    private bool textColorCreated = false;
+    private bool textCloseCreated = false;
+    private bool textCongratulationsCreated = false;
 
+    private void Start()
+    {
+        StartCoroutine(spawnText(0, "One Pixel", new Vector3(0, yOffsetForText, 0)));
+    }
     // Update is called once per frame
     void Update()
     {
@@ -36,12 +51,39 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Text creation code
+        // Text creation code - I know this is all very hardcoded and hacky, but it's a 48 hour game jam and I don't have time to make it better
         if (playerHasMovedEver && !text1Created)
         {
             StartCoroutine(spawnText(0, "I don't belong here."));
             StartCoroutine(spawnText(chainTextDelay, "I need to find where I belong."));
             text1Created = true;
+        } else if (!textHillCreated && desiredRotation > 0f)
+        {
+            StartCoroutine(spawnText(0, "A hill?"));
+            textHillCreated = true;
+        } else if (textHillCreated && !textSteeperCreated && desiredRotation > 31f)
+        {
+            StartCoroutine(spawnText(0, "I'm not sure if I can make it."));
+            textSteeperCreated = true;
+        }
+        else if (textSteeperCreated && !textSteepestCreated && desiredRotation > 46f)
+        {
+            StartCoroutine(spawnText(0, "So steep!"));
+            textSteepestCreated = true;
+        }
+        else if (textSteepestCreated && !textMadeItCreated && desiredRotation < 10f && transform.position.x > 5f)
+        {
+            StartCoroutine(spawnText(0, "Phew, the top flattens out."));
+            textMadeItCreated = true;
+        }
+        else if (textSteepestCreated && !textColorCreated && transform.position.x >= 11.5f)
+        {
+            StartCoroutine(spawnText(0, "What are these colors?"));
+            textColorCreated = true;
+        } else if (textColorCreated && !textCloseCreated && transform.position.x >= 18f)
+        {
+            StartCoroutine(spawnText(0, "I think I'm close to where I belong."));
+            textCloseCreated = true;
         }
 
         // Movement code
@@ -54,10 +96,26 @@ public class PlayerMovement : MonoBehaviour
         }
         float newXPos = Mathf.Min(maxXPos, transform.position.x + horizontalMove * Time.fixedDeltaTime);
         float newYPos = transform.position.y + verticalMove * Time.fixedDeltaTime;
+        if (newXPos <= minXPos && !textLeftBoundCreated)
+        {
+            if (!textLeftBoundCreated)
+            {
+                StartCoroutine(spawnText(0, "This direction feels wrong."));
+                StartCoroutine(spawnText(chainTextDelay, "Moving to the right feels better."));
+                textLeftBoundCreated = true;
+            }
+            newXPos = minXPos; // Block the player from moving any farther left
+        } else if (!textDownhillCreated && FindObjectsOfType<Text>().Length == 0 && newXPos < transform.position.x && desiredRotation > 0f)
+        {
+            StartCoroutine(spawnText(0, "I can't give up."));
+            textDownhillCreated = true;
+        }
         transform.position = new Vector3(newXPos, newYPos, transform.position.z);
-        if (newXPos >= maxXPos)
+        if (!textCongratulationsCreated && newXPos >= maxXPos)
         {
             controlsEnabled = false;
+            StartCoroutine(spawnText(0, "Congratulations!", new Vector3(0, yOffsetForText, 0)));
+            textCongratulationsCreated = true;
         }
 
         // Rotation code
